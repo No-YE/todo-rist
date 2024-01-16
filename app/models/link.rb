6bd9c2b838829100753163c9812be89e20e47591
No-YE <nye7181@gmail.com>
2024-01-16 23:18:09 +0900

    add due date validates

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,8 @@ class Link < ApplicationRecord
 
   validates :url, presence: true, uniqueness: { scope: :user_id }
   validates :user_id, presence: true
+  validates :due_date,
+            comparison: { greater_than_or_equal_to: -> { Time.current.to_date }, allow_nil: true }
 
   after_update_commit do
     broadcast_replace_to 'links', locals: { link: self, current_user: user }
