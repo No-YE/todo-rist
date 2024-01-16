fb820e6bc65552c18ee9d4b98b4bdf3a733b0f5f
No-YE <nye7181@gmail.com>
2024-01-16 22:34:55 +0900

    add concurrency key to remind job

--- app/jobs/links/remind_job.rb
+++ app/jobs/links/remind_job.rb
@@ -3,6 +3,11 @@
 class Links::RemindJob < ApplicationJob
   queue_as :default
 
+  good_job_control_concurrency_with(
+    total_limit: 1,
+    key: -> { "Links::RemindJob-#{arguments.first.id}" },
+  )
+
   def perform(reminder_setting)
     links = Link.remind_target(reminder_setting.criteria_days)
     Links::RemindNotification.with(links:, reminder_setting:).deliver(reminder_setting.user)
